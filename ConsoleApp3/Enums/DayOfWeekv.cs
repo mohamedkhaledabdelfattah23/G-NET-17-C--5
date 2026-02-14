835939ce65d711b29b414f4146fe9647c3a38d63
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-02-14 18:43:41 +0200

    Question 01

---
+++ ConsoleApp3/Enums/DayOfWeekv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Enums
+{
+    enum DayOfWeekv
+    {
+
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace ConsoleApp3
+﻿using ConsoleApp3.Enums;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Question 01
+            // Console.WriteLine("Hello, World!");
+
+
+
+            // Console.Write("Enter a day number (0-6):");
+            // DayOfWeekv day;
+
+            //bool isparsed = Enum.TryParse(Console.ReadLine(), out day);
+
+
+            // switch (day)
+            // {
+            //     case DayOfWeekv.Saturday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine(" It is the Weekend ");
+            //         break;
+            //     case DayOfWeekv.Sunday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("WorkDay");
+            //         break;
+            //     case DayOfWeekv.Monday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("WorkDay");
+            //         break;
+            //     case DayOfWeekv.Tuesday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("WorkDay");
+            //         break;
+            //     case DayOfWeekv.Wednesday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("Workday");
+            //         break;
+            //     case DayOfWeekv.Thursday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("WorkDay");
+            //         break;
+            //     case DayOfWeekv.Friday:
+            //         Console.WriteLine($"Day : {day}");
+            //         Console.WriteLine("Weekend");
+            //         break;
+
+
+            // } 
+            #endregion
+
+
+
+
+
+
+        }    
     }
 }
